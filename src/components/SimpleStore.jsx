e8b416209a825ab8b1a4ff3c18525de96f646ad6
atroopa <atroopa@gmail.com>
2023-05-14 19:10:20 +0330

    create function accountChangeHandler-

--- src/components/SimpleStore.jsx
+++ src/components/SimpleStore.jsx
@@ -2,8 +2,8 @@ import React, {useState} from 'react';
 
 const SimpleStore = () => {
 
-    errorMessage = 'new Value';
-    setErrorMessage('new value');
+    //errorMessage = 'new Value';
+    //setErrorMessage('new value');
 
     const [errorMessage, setErrorMessage]             = useState(null);
     const [defaultAccount , setDefaultAccount]        = useState(null);
@@ -14,13 +14,32 @@ const SimpleStore = () => {
     const [contrsct, setContrsct]                     = useState(null);
 
     const connectWalletHandler = () => {
+        if (window.ethereum){
+            window.ethereum.request({methos: 'eth_requestAccounts'})
+                .then(result => {
+                    accountChangeHandler(result[0]);
+                    setConnectButtonText("Wallet Connected");
+                })
 
+        }else{
+            setErrorMessage("need To Install MetaMask");
+        }
+    }
+
+    const accountChangeHandler = (newAccount) => {
+        setDefaultAccount(newAccount);
     }
 
     return (
         <div className='flex flex-col items-center py-11'>
             <h3 className='text-2xl font-bold'>{"Get/Set interaction witch contract"}</h3>
-            <button onClick={connectWalletHandler}>{connectButtonText}</button>
+            <button class="my-10 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white font-bold py-2 px-4 rounded-full"onClick={connectWalletHandler}>
+                {connectButtonText}
+            </button>
+            <h3 className='text-2xl'> Address: {defaultAccount}</h3>
+
+            {errorMessage}
+
         </div>
     );
 };
